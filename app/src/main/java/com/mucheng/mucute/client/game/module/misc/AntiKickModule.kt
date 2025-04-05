9469c9710dc6d2f3891e93939626053fd36166a4
john220099 <148237962+John220099@users.noreply.github.com>
2025-04-05 20:03:02 +0200

    Update AntiKickModule.kt

--- app/src/main/java/com/mucheng/mucute/client/game/module/misc/AntiKickModule.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/module/misc/AntiKickModule.kt
@@ -3,17 +3,19 @@ package com.mucheng.mucute.client.game.module.misc
 import com.mucheng.mucute.client.game.InterceptablePacket
 import com.mucheng.mucute.client.game.Module
 import com.mucheng.mucute.client.game.ModuleCategory
+import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
 import org.cloudburstmc.protocol.bedrock.packet.DisconnectPacket
 
 class AntiKickModule : Module("AntiKick", ModuleCategory.Misc) {
 
-override fun beforeClientBound(interceptablePacket: InterceptablePacket) {
+override fun afterPacketBound(packet: BedrockPacket) {
     if (!isEnabled) {
       return
-    }        
-    val packet = interceptablePacket.packet    
+    }
     
-    if (packet is DisconnectPacket) {
+    val packet = BedrockPacket
+    
+    if (packet is DisconnectPacket && disconnectPacketValue) {
         return 
         }         
     } 
