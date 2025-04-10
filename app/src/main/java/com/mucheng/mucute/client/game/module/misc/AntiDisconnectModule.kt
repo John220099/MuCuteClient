a8e99ca8b72c498a17d620af135817efcfc1df6e
john220099 <148237962+John220099@users.noreply.github.com>
2025-04-10 20:59:07 +0200

    Add files via upload

---
+++ app/src/main/java/com/mucheng/mucute/client/game/module/misc/AntiDisconnectModule.kt
@@ -0,0 +1,20 @@
+package com.mucheng.mucute.client.game.module.misc
+
+import com.mucheng.mucute.client.game.InterceptablePacket
+import com.mucheng.mucute.client.game.Module
+import com.mucheng.mucute.client.game.ModuleCategory
+import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
+import org.cloudburstmc.protocol.bedrock.packet.DisconnectPacket
+
+class AntiDisconnectModule : Module("AntiDisconnect", ModuleCategory.Misc) {
+
+    override fun beforePacketBound(interceptablePacket: InterceptablePacket) {
+        val packet = interceptablePacket.packet
+        if (packet is PlayerAuthInputPacket) {
+            if (packet is DisconnectPacket) {
+                interceptablePacket.intercept()
+                return
+            }
+        }
+    }
+}
