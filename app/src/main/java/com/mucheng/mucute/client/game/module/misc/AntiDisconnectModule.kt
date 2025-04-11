d6a5f0771177f99c8fe0ce61af52bc75523b5d53
john220099 <148237962+John220099@users.noreply.github.com>
2025-04-11 10:07:02 +0200

    Update AntiDisconnectModule.kt

--- app/src/main/java/com/mucheng/mucute/client/game/module/misc/AntiDisconnectModule.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/module/misc/AntiDisconnectModule.kt
@@ -3,19 +3,27 @@ package com.mucheng.mucute.client.game.module.misc
 import com.mucheng.mucute.client.game.InterceptablePacket
 import com.mucheng.mucute.client.game.Module
 import com.mucheng.mucute.client.game.ModuleCategory
-import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
 import org.cloudburstmc.protocol.bedrock.packet.DisconnectPacket
+import org.cloudburstmc.protocol.bedrock.packet.TransferPacket
 
 class AntiDisconnectModule : Module("AntiDisconnect", ModuleCategory.Misc) {
+
+    private val BlockDisconnectPacket by boolValue("Block_DisconnectPacket", false)
+    private val BlockTransferPacket by boolValue("Block_TransferPacket", false)
 
     override fun beforePacketBound(interceptablePacket: InterceptablePacket) { 
         if (!isEnabled) {
             return
         }
-
         val packet = interceptablePacket.packet
-        if (packet is PlayerAuthInputPacket) {
             if (packet is DisconnectPacket) {
+            if (BlockDisconnectPacket) {
+                interceptablePacket.intercept()
+                return
+            }
+        }
+            if (packet is TransferPacket) {
+            if (BlockTransferPacket) {
                 interceptablePacket.intercept()
                 return
             }
